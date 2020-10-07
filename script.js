851646f38b51473cfe66930c2b812851fb00dd4b
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-3.local>
2020-10-07 16:30:35 -0600

    removed console logs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let divs = document.getElementsByTagName("div");
 window.addEventListener("scroll", e => {
   let cutoff = divs[0].getBoundingClientRect().y+1;
 
-  console.log(divs[0].getBoundingClientRect());
+
   for (let i = 0; i < divs.length; i++) {
     if (divs[i].getBoundingClientRect().y < cutoff && i > 0) {
       divs[i].classList.add("hide");
